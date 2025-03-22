421ad3f2cead966f430b77bbb71a13b2a9ce68a6
Solus-QE <instance.workspace.realms@gmail.com>
2025-03-22 23:52:10 +0000

    added welcome screen, centered main screen

---
+++ start.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("startButton").addEventListener("click", function() {
+        window.location.href = "main.html"; // Redirect to main content page
+    });
+});
